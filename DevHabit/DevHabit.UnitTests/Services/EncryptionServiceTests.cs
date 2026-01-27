678cb43307ccd019f9dd4d3403fe8cb68cec9d2b
unknown <giovanna.vasques1101@hotmail.com>
2026-01-27 14:11:59 -0300

    chore: creating UNIT TESTS (xUnit Test Project)

---
+++ DevHabit/DevHabit.UnitTests/Services/EncryptionServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using DevHabit.Api.Services;
+using DevHabit.Api.Settings;
+using Microsoft.Extensions.Options;
+
+namespace DevHabit.UnitTests.Services;
+
+public sealed class EncryptionServiceTests
+{
+    private readonly EncryptionService _encryptionService;
+
+    public EncryptionServiceTests()
+    {
+        IOptions<EncryptionOptions> options = Options.Create(new EncryptionOptions 
+        { 
+            Key = Convert.ToBase64String(RandomNumberGenerator.GetBytes(32))
+        });
+
+        _encryptionService = new EncryptionService(options);
+    }
+
+    [Fact]
+    public void Decrypt_ShouldReturnPlainText_WhenDecryptingCorrectCipherText()
+    {
+        // Arrange
+        const string plainText = "sensitive data";
+        string cipherText = _encryptionService.Encrypt(plainText);
+
+        // Act
+        string decryptedText = _encryptionService.Decrypt(cipherText);
+
+        // Assert
+        Assert.Equal(plainText, decryptedText);
+    }
+}
