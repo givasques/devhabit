f550b5211276d85d69f964a26f6db2b6ff1aeaf4
unknown <giovanna.vasques1101@hotmail.com>
2026-02-04 10:23:07 -0300

    chore: fixing the client app

--- client/devhabit-ui/src/features/auth/Signup.tsx
+++ client/devhabit-ui/src/features/auth/Signup.tsx
@@ -1,26 +1,21 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import SignupForm from './SignupForm';
-import { register } from '../../api/auth';
-//import { useAuth } from '../../context/AuthContext';
 
 export default function Signup() {
   const [error, setError] = useState<string | null>(null);
   const navigate = useNavigate();
-  //const { login } = useAuth();
 
   const handleSignup = async (
-    name: string,
-    email: string,
-    password: string,
-    confirmPassword: string
+    _name: string,
+    _email: string,
+    _password: string,
+    _confirmPassword: string
   ) => {
     try {
       setError(null);
-      const response = await register({ name, email, password, confirmPassword });
 
-      // Store tokens and redirect
-      //login(response.accessToken, response.refreshToken);
+      // Auth0 está lidando com registro/login, então não chamamos register aqui
       navigate('/dashboard');
     } catch (err) {
       setError('An unexpected error occurred. Please try again.');
